60158a1669fd2e981abf913db8608a353ab3640f
Aleksei Kurtukov <kav.prok@gmail.com>
2024-07-19 16:09:27 +0700

    fix: fix scroll lock when clicking on menu link at desktop window size

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,32 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const body = document.querySelector("body");
-  const headerMenu = document.querySelector(".header__menu");
-  const headerMenuBurger = document.querySelector(".header__menu-burger");
+document.addEventListener('DOMContentLoaded', function () {
+  const body = document.querySelector('body');
+  const headerMenu = document.querySelector('.header__menu');
+  const headerMenuBurger = document.querySelector('.header__menu-burger');
   const mobileWidth = 768;
 
   /* Burger Menu */
 
   function toggleBurgerMenu() {
-    headerMenuBurger.classList.toggle("burger--open");
-    headerMenu.classList.toggle("header__menu--open");
-    body.classList.toggle("no-scroll");
+    headerMenuBurger.classList.toggle('burger--open');
+    headerMenu.classList.toggle('header__menu--open');
+    body.classList.toggle('no-scroll');
   }
 
-  headerMenuBurger.addEventListener("click", function () {
+  headerMenuBurger.addEventListener('click', function () {
     toggleBurgerMenu();
   });
 
-  headerMenu.addEventListener("click", (e) => {
-    if (e.target.classList.contains("header__menu-link")) {
+  headerMenu.addEventListener('click', (e) => {
+    if (
+      e.target.classList.contains('header__menu-link') &&
+      window.innerWidth <= mobileWidth
+    ) {
       toggleBurgerMenu();
     }
   });
 
-  window.addEventListener("resize", function () {
+  window.addEventListener('resize', function () {
     if (window.innerWidth >= mobileWidth) {
-      body.classList.remove("no-scroll");
-      headerMenuBurger.classList.remove("burger--open");
-      headerMenu.classList.remove("header__menu--open");
+      body.classList.remove('no-scroll');
+      headerMenuBurger.classList.remove('burger--open');
+      headerMenu.classList.remove('header__menu--open');
     }
   });
 });
